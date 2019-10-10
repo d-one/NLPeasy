ac3024e9e176e21e07c5062793e0a1d5869d1ed2
Philipp Thomann <philipp.thomann@d-one.ai>
2019-10-10 23:40:26 +0200

    added end to end test

--- tests/test_nlpeasy.py
+++ tests/test_nlpeasy.py
@@ -6,20 +6,36 @@
 import pytest
 
 
-from nlpeasy import pipeline
+import nlpeasy as ne
+import pandas as pd
 
+@pytest.fixture(scope="session")
+def elk():
+    """The ELK stack class to use.
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+    Reuse the same elk among the whole testing session by setting scope="session"
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    elk = ne.connect_elastic(dockerPrefix='nlp', elkVersion='7.4.0', mountVolumePrefix=None)
+    return elk
 
 
-def test_content(response):
+def test_end_to_end(elk):
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    # read data as Pandas data frame
+    nips = pd.read_pickle("data_raw/nips.pickle")
+
+    # setup stages in the NLP pipeline and set textfields
+    pipeline = ne.Pipeline(index='nips', textCols=['message','title'], dateCol='year', elk=elk)
+
+    pipeline += ne.RegexTag(r'\$([^$]+)\$', ['message'], 'math')
+    # pipeline += ne.VaderSentiment('message', 'sentiment')
+    # pipeline += ne.SpacyEnrichment(cols=['message','title'])
+
+    # do the pipeline
+    nips_enriched = pipeline.process(nips.head(100), writeElastic=True)
+
+    # Create Kibana Dashboard of all the columns
+    #pipeline.create_kibana_dashboard()
+
+    # open Kibana in webbrowser
+    #elk.show_kibana()
